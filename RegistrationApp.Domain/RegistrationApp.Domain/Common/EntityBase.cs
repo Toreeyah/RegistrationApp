081d20b89f934dbca2146a7ea4992131b34f17a9
Victoria Ritah <72909509+Toreeyah@users.noreply.github.com>
2023-09-08 13:44:46 +0300

    Add files via upload

---
+++ RegistrationApp.Domain/RegistrationApp.Domain/Common/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationApp.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public Guid Id { get; set; }   
+        public DateTime CreatedOn { get;set; }
+
+    }
+}
